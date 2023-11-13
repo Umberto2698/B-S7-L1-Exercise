3f6779054812df77cf4ddb75fa525a7fb94222e4
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-13 15:51:59 +0100

    Added security second stage

--- src/main/java/lezione26/security/JWTTools.java
+++ src/main/java/lezione26/security/JWTTools.java
@@ -3,6 +3,7 @@ package lezione26.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lezione26.enteties.User;
+import lezione26.exceptions.UnauthorizedException;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
@@ -18,5 +19,18 @@ public class JWTTools {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + milliseconds * 7))
                 .signWith(Keys.hmacShaKeyFor(secret.getBytes())).compact();
+    }
+
+    public void verifyToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+
+        } catch (Exception ex) {
+            throw new UnauthorizedException("Invalid token, please log in.");
+        }
+    }
+
+    public String extractIdFroToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(secret.getBytes())).build().parseClaimsJws(token).getBody().getSubject();
     }
 }
