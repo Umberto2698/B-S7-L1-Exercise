ec6e989c37e8fd0bf45753c9de4d06aece887e79
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-13 14:51:57 +0100

    start

---
+++ src/main/java/lezione26/Lezione26Application.java
@@ -0,0 +1,13 @@
+package lezione26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lezione26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lezione26Application.class, args);
+	}
+
+}
---
+++ src/test/java/lezione26/Lezione26ApplicationTests.java
@@ -0,0 +1,13 @@
+package lezione26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lezione26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
